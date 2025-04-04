e564469f350fdb296cc4132949730b5013b938b6
lbfatcgf <lbfatcgf@outlook.com>
2025-04-04 22:14:08 +0800

    优化配置初始化

--- common/config/viper.go
+++ common/config/viper.go
@@ -3,8 +3,6 @@ package config
 import (
 	"sync"
 
-
-
 	"github.com/spf13/viper"
 )
 
@@ -22,27 +20,29 @@ func ConfigReader() *viper.Viper {
 	return configReader
 }
 
+var onceInitConfig func()
+
 // InitConfig 初始化配置,
 //
 // 应该在main函数中调用，且要在调用Conf()和GetConfigReader()之前执行,只执行一次。
-func InitConfig(confPath,confName,confType string) {
-	sync.OnceFunc(func() {
-		v := viper.New()
-		configReader = v
-		configReader.SetConfigName(confName)
-		configReader.AddConfigPath(confPath)
-		configReader.SetConfigType(confType)
-		err := configReader.ReadInConfig()
-		if err != nil {
-			panic(err)
-		}
-		err = configReader.Unmarshal(&appConfig)
-	
-		if err != nil {
-			panic(err)
-		}
-	})
-	
+func InitConfig(confPath, confName, confType string) {
+	if onceInitConfig == nil {
+		onceInitConfig = sync.OnceFunc(func() {
+			v := viper.New()
+			configReader = v
+			configReader.SetConfigName(confName)
+			configReader.AddConfigPath(confPath)
+			configReader.SetConfigType(confType)
+			err := configReader.ReadInConfig()
+			if err != nil {
+				panic(err)
+			}
+			err = configReader.Unmarshal(&appConfig)
+
+			if err != nil {
+				panic(err)
+			}
+		})
+	}
+	onceInitConfig()
 }
-
-
