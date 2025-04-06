553c6ca4d32054a8ba2b37ae79959c162ca3dd5e
lbfatcgf <lbfatcgf@outlook.com>
2025-04-06 13:20:43 +0800

    修复日志bug

--- common/logger/log.go
+++ common/logger/log.go
@@ -157,7 +157,7 @@ func checkLogFileName() {
 	newFilePtah := fp.Join(logdir, tools.SafeFilePath(config.Conf().Name+"-"+time.Now().Format("2006-01-02")+".log"))
 	if newFilePtah != logFile.Name() {
 		logFile.Close()
-		nf, err := os.OpenFile(fp.Join(config.Conf().LogDir, newFilePtah), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		nf, err := os.OpenFile(newFilePtah, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 		if err != nil {
 			panic(err)
 		}
